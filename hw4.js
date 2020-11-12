f09fa4658c2e2a83190d29ed0eb81eeea8616ddd
Anastasia <anastasiareiz@gmail.com>
2020-11-12 17:39:13 +0300

    delete unnecessary line

--- hw4.js
+++ hw4.js
@@ -1,47 +1,27 @@
 'use strict';
 
 const splitAndMerge = (str, sp) =>
-    str.split(" ").map(elem => elem.split("").join(sp)).join(" ");
+    str.split(" ").map(elem => elem.split("").join(sp)).join(" ")
 
-const convert = (hash) => {
-    return Object.entries(hash);
-}
+const convert = (hash) => Object.entries(hash)
 
-console.log(convert({name: 'Jeremy', age: 24, role: 'Software Engineer'}));
+const toCamelCase = (str) => str.replace(/[-_](.)/g, (char) => char[1].toUpperCase())
 
-const toCamelCase = (str) => {
-    return str.replace(/[-_](.)/g, (char) => char[1].toUpperCase());
-}
+const reverseEachWord = (str) => str.split(" ").map((word) => word.split("").reverse().join("")).join(" ")
 
-const reverseEachWord = (str) => {
-    return str.split(" ").map((word) => word.split("").reverse().join("")).join(" ");
-}
+const stringExpansion = (str) => !str.length ? '' : str.replace(/(\d)+([A-Za-zА-Яа-я])/g, (match, number, letter) =>
+    letter.repeat(number)
+)
 
-const stringExpansion = (str) => {
-    if (!str.length) return "";
-    return str.replace(/(\d)+([A-Za-zА-Яа-я])/g, (match, number, letter) =>
-        letter.repeat(number)
-    )
-}
+const largest = (...args) => Math.max.apply(null, args)
 
-const largest = (...args) => {
-    return Math.max.apply(null, args);
-}
+const smallest = (...args) => Math.min.apply(null, args)
 
-const smallest = (...args) => {
-    return Math.min.apply(null, args);
-}
+const transform = (baseArray) => baseArray.map((number) => () => number)
 
-const transform = (baseArray) => baseArray.map((number) => () => number);
+const sum = (...arg) => !arg.length ? 0 : arg[0] + sum.apply(null, arg.slice(1))
 
-const sum = (...arg) => {
-    if (!arg.length) {
-        return 0;
-    }
-    return arg[0] + sum.apply(null, arg.slice(1));
-}
-
-const countDown = time => {
+const countDown = time =>
     setTimeout(() => {
         if (time >= 0) {
             console.log(time);
@@ -50,7 +30,6 @@ const countDown = time => {
             console.log(0)
         }
     }, 1000);
-}
 
 countDown(3);
 
